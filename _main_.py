8d789cebea887fdcc783a92998967dd58990f92e
Fuzzy <cassidy.off@outlook.com.au>
2022-12-11 18:46:24 +0100

    Create _main_.py

---
+++ _main_.py
@@ -0,0 +1,6 @@
+import os; from os import system as sys
+print(
+    ' English >> Terranian.'
+    '\n For help visit https://www.github.com/FuzzyTheFoxx/Terranian-Translator'
+)
+sys('python3 exec2.py')
