f3e3950dfa67a06c4acf63c9b64a32ab5243e839
Fuzzy <cassidy.off@outlook.com.au>
2022-12-12 18:19:19 +0100

    Delete _main_.py

--- _main_.py
+++
@@ -1,6 +0,0 @@
-import os; from os import system as sys
-print(
-    ' English >> Terranian.'
-    '\n For help visit https://www.github.com/FuzzyTheFoxx/Terranian-Translator'
-)
-sys('python3 exec2.py')
